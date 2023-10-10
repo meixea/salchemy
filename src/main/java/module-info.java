9fcb7b1a2e85e56a6f27502699545ce935eed4bc
Mikhail <mixael@bk.ru>
2023-10-10 11:30:46 +0600

    Start.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.meixea.salchemy {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens org.meixea.salchemy.model to javafx.fxml;
+    exports org.meixea.salchemy.model;
+    exports org.meixea.salchemy.view;
+    opens org.meixea.salchemy.view to javafx.fxml;
+    exports org.meixea.salchemy.controller;
+    opens org.meixea.salchemy.controller to javafx.fxml;
+}
---
+++ src/main/java/org/meixea/salchemy/model/Main.java
@@ -0,0 +1,9 @@
+package org.meixea.salchemy.model;
+
+import org.meixea.salchemy.view.AlchemyApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        AlchemyApplication.main(args);
+    }
+}
